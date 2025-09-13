06f38a0d5bcbf6b053c95ecd2cbd17caa441b3cc
Muhammad Mehedi Hasan <mehedihasan199908@gmail.com>
2025-09-13 23:53:25 +0600

    updated routes

---
+++ routes/web.php
@@ -0,0 +1,115 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    $practiceUrl = route('practice.view', ['keyF' => 'first', 'keyL' => 100]);
+    dd($practiceUrl);
+
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/product/{id}', function (string $id) {
+    return "product id=$id";
+});
+
+/*optional parameter*/
+
+Route::get('/product/{category?}', function (string $category = null) {
+    return "product for category=$category";
+});
+
+/*parameter must be number*/
+
+Route::get('/product/{id}', function (string $id) {
+    return "Checking for number!! Works, Number: $id";
+})->whereNumber('id');
+
+/*parameter must be letters*/
+
+Route::get('/letter/{let}', function (string $let) {
+    return "Checking for only letters!! works, letter: $let";
+})->whereAlpha("let");
+
+/*parameter containing both letters & numbers or only letters or numbers*/
+Route::get("/alphanumeric/{alphanum}", function (string $alphanum) {
+    return "Checking for both letters & numbers!! works, $alphanum";
+})->whereAlphaNumeric('alphanum');
+
+/*parameter containing specific keywords*/
+Route::get('/specific/{keywords}', function (string $keyword) {
+    return "Checking for specific keywords!! Works, $keyword";
+})->whereIn("keywords", ["abc", "def", "ghi"]);
+
+
+/*REGEX*/
+
+/*parameter containing only lowercase letters*/
+Route::get('/checklower/{keyword}', function (string $keyword) {
+    return "Checking for lower-case!! Works, $keyword";
+})->where('keyword', '[a-z]+');
+
+/*parameter containing only uppercase letters*/
+Route::get('/checkupper/{keyword}', function (string $keyword) {
+    return "Checking for upper-case!! Works, $keyword";
+})->where('keyword', '[A-Z]+');
+
+Route::get('/{lang}/product/{id}', function (string $lang, string $id) {
+    return "Language: $lang. Product id: $id";
+})->where(['lang' => '[a-z]{2}', 'id' => '\d{4,}']);
+
+/*parameter containing special characters*/
+Route::get('/search/{keyword}', function (string $keyword) {
+    return "Check: $keyword";
+})->where('keyword', '.+');
+
+
+/*named routes*/
+
+Route::get('/{keyF}/practice/{keyL}', function (string $keyF, $keyL) {
+
+})->name("practice.view");
+
+/*redirecting routes*/
+Route::get('/user/profile', function () { })->name('profile');
+
+Route::get('/current-user', function () {
+
+    return redirect()->route('profile');
+
+    //alternative
+    //return to_route('profile');
+
+});
+
+/*grouping routes*/
+Route::prefix('/admin')->group(function () {
+    route::get('/users', function () {
+        return '/admin/users';
+    });
+});
+
+Route::name('admin.')->group(function () {
+    Route::get('/users', function () {
+        return 'users';
+    })->name('users');
+});
+
+/*fallback routes*/
+Route::fallback((function () {
+    return 'fallback';
+}));
+
+/*adding 2 nums from router*/
+// Route::get('/sumNum/{numOne}/{numTwo}', function (string $numOne, string $numTwo) {
+//     return "$numOne + $numTwo = " . ($numOne + $numTwo);
+// })->where(['numOne' => '\d+', 'numTwo' => '\d+']);
+
+Route::get('/sumNum/{numOne}/{numTwo}', function (string $numOne, string $numTwo) {
+    return "$numOne + $numTwo = " . ($numOne + $numTwo);
+})->whereNumber(['numOne', 'numTwo']);
+
